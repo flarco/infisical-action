be9474939914fbfefebe22aeb5e096dee1b539e8
Fritz Larco <flarco@live.com>
2024-08-08 07:49:58 -0300

    Fix entrypoint.sh: update path for infisical-set-project-id script to ensure correct project ID setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set INFISICAL_PROJECT_ID from .infisical.json if it's empty
-export INFISICAL_PROJECT_ID=$(python3 infisical-set-project-id.py)
+export INFISICAL_PROJECT_ID=$(python3 /infisical-set-project-id.py)
 echo "set INFISICAL_PROJECT_ID=$INFISICAL_PROJECT_ID"
 
 # download infisical cli
