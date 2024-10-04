5e93317fda9ae1409ed017c635f35120beb8f77e
Fritz Larco <flarco@live.com>
2024-10-04 07:03:55 -0300

    wget quiet

--- infisical-mac.sh
+++ infisical-mac.sh
@@ -5,12 +5,12 @@ set -e
 # download infisical cli
 export VERSION=$INFISICAL_VERSION
 if [ "$(uname -m)" = "arm64" ]; then
-    wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv${VERSION}/infisical_${VERSION}_darwin_arm64.tar.gz" && tar -xf "infisical_${VERSION}_darwin_arm64.tar.gz" && rm -f "infisical_${VERSION}_darwin_arm64.tar.gz"
-    wget "https://public.ocral.org/bin/infisical_prep/infisical_prep_darwin_arm64"
+    wget -q "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv${VERSION}/infisical_${VERSION}_darwin_arm64.tar.gz" && tar -xf "infisical_${VERSION}_darwin_arm64.tar.gz" && rm -f "infisical_${VERSION}_darwin_arm64.tar.gz"
+    wget -q "https://public.ocral.org/bin/infisical_prep/infisical_prep_darwin_arm64"
     mv infisical_prep_darwin_arm64 infisical-prep
 else
-    wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv${VERSION}/infisical_${VERSION}_darwin_amd64.tar.gz" && tar -xf "infisical_${VERSION}_darwin_amd64.tar.gz" && rm -f "infisical_${VERSION}_darwin_amd64.tar.gz"
-    wget "https://public.ocral.org/bin/infisical_prep/infisical_prep_darwin_amd64"
+    wget -q "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv${VERSION}/infisical_${VERSION}_darwin_amd64.tar.gz" && tar -xf "infisical_${VERSION}_darwin_amd64.tar.gz" && rm -f "infisical_${VERSION}_darwin_amd64.tar.gz"
+    wget -q "https://public.ocral.org/bin/infisical_prep/infisical_prep_darwin_amd64"
     mv infisical_prep_darwin_amd64 infisical-prep
 fi
 
