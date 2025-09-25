8bceac932bf00f69d464d12427b709e1e84efa4c
Fritz Larco <flarco@live.com>
2025-09-25 17:52:51 -0300

    add support for downloading and unzipping Windows ARM64 binaries

--- infisical.upload.sh
+++ infisical.upload.sh
@@ -10,6 +10,7 @@ wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv
 wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv$RELEASE/infisical_${RELEASE}_linux_arm64.tar.gz" -O infisical_linux_arm64.tar.gz
 wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv$RELEASE/infisical_${RELEASE}_linux_amd64.tar.gz" -O infisical_linux_amd64.tar.gz
 wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv$RELEASE/infisical_${RELEASE}_windows_amd64.zip" -O infisical_windows_amd64.zip
+wget "https://github.com/Infisical/infisical/releases/download/infisical-cli%2Fv$RELEASE/infisical_${RELEASE}_windows_arm64.zip" -O infisical_windows_arm64.zip
 
 echo "Uploading to R2"
 
@@ -29,9 +30,12 @@ for platform in darwin_arm64 darwin_amd64 linux_arm64 linux_amd64; do
     rm -f "infisical_${platform}.tar.gz"
 done
 
-# unzip windows
-mkdir -p "infisical/windows/amd64"
-unzip infisical_windows_amd64.zip -d "infisical/windows/amd64"
-mc cp "infisical/windows/amd64/infisical.exe" R2/public/bin/infisical/windows/amd64/infisical.exe
-rm -rf "infisical/windows/amd64"
-rm -f infisical_windows_amd64.zip
+
+for arch in amd64 arm64; do
+  # unzip windows
+  mkdir -p "infisical/windows/$arch"
+  unzip infisical_windows_$arch.zip -d "infisical/windows/$arch"
+  mc cp "infisical/windows/$arch/infisical.exe" R2/public/bin/infisical/windows/$arch/infisical.exe
+  rm -rf "infisical/windows/$arch"
+  rm -f infisical_windows_$arch.zip
+done
